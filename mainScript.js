b8cb20eba7f073378430b6749a3ccef7bdbab26b
Evelina <aoiichann@outlook.com>
2020-04-04 20:22:52 +0300

    Implemented most of actual functionality, removed useless things

--- mainScript.js
+++ mainScript.js
@@ -1,28 +1,37 @@
 const electron = require('electron');
 const fs = require('fs');
 
-document.getElementById("yeet").innerHTML = "yeet";
-console.log(document);
 var allTimers = [];
-
+var expiredTimers = [];
 
 document.getElementById("newTimerButton").addEventListener("click", newTimer);
 
 
 
 function newTimer() {
-	/*let alarmTimeInMin = document.getElementById("timerTime").value;
-	let alarmName = document.getElementById("timerName").value;
-	let alarmColor = document.getElementById("timerColor").value;*/
 	allTimers.push({
-		time: Date.now() +  document.getElementById("timerTime").value * 60000,
+		time: new Date(document.getElementById("timerTime").value * 60000), //This will keep track of when the alarm should ring (will have a small epoch value)
+		alarmTime: Date.now() + document.getElementById("timerTime").value * 60000, //This stores the timestamp of the time when the alarm should ring
+		origTime: new Date(new Date(document.getElementById("timerTime").value * 60000) - Date.now()), //Used to tell what the original time was upon alarm expiry
 		name: document.getElementById("timerName").value,
 		color: document.getElementById("timerColor").value
 	});
 	writeTimers()
-
 }
 
+function refreshTimers() {
+	allTimers.forEach(timer => {
+		timer.time = new Date(timer.alarmTime - Date.now());
+		if (timer.time < 0) {
+			expiredTimers.push(timer);
+			let notif = new Notification("Expired timer: " + timer.name, {
+				body: "The timer named " + timer.name + " (for " +  stringTime(timer.origTime) +") has expired."
+			})
+			delete (notif);
+			allTimers.splice(allTimers.indexOf(timer), 1);
+		}
+	})
+}
 
 function deleteTimer(e) {
 	allTimers.splice(e.id, 1);
@@ -30,7 +39,13 @@ function deleteTimer(e) {
 	writeTimers();
 }
 
-
+function stringTime(time) {
+	let collect = " "
+	console.log(typeof time)
+	if (time.getMinutes() > 0) { collect += time.getMinutes() + "min " }
+	if (time.getSeconds() > 0) { collect += time.getSeconds() + "sec " }
+	return collect;
+}
 
 deleteButtons = []
 function writeTimers() {
@@ -39,7 +54,7 @@ function writeTimers() {
 		let curTimer = document.getElementById("timerDiv").appendChild(document.createElement("div"));
 		curTimer.className = "timer";
 		curTimer.innerHTML = ("Timer #" + allTimers.indexOf(timer) +
-			": <em>" + timer.name + "</em>, in: " + (timer.time - Date.now()) +
+			": <em>" + timer.name + "</em>, in: " + stringTime(timer.time)/*(timer.time - Date.now())*/ +
 			("&nbsp;&nbsp;&nbsp;&nbsp;") +
 			'<button class="timerDeleter" id="' + allTimers.indexOf(timer) + '">Delete this timer</button>');
 		curTimer.style.backgroundColor = timer.color;
@@ -60,5 +75,6 @@ let myNotification = new Notification('Title', {
 })
 */
 window.setInterval(function () {
+	refreshTimers()
 	writeTimers();
-}, 2000);
+}, 1000);
